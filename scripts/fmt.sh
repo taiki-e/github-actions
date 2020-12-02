e85b4c59eb2feabbc9234d3aba6fbaf89b800426
Taiki Endo <te316e89@gmail.com>
2020-12-02 19:30:28 +0900

    Use scripts/fmt.sh in CI

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 # Format all scripts.
+#
+# Note: This script requires shfmt and clang-format.
 
 set -euo pipefail
 IFS=$'\n\t'
 
 cd "$(cd "$(dirname "${0}")" && pwd)"/..
 
-shfmt -l -w ./**/*.sh
+if [[ -n "${CI:-}" ]]; then
+  shfmt -d ./**/*.sh
+else
+  shfmt -l -w ./**/*.sh
+fi
 
 clang-format -i ./*/*.js
+if [[ -n "${CI:-}" ]]; then
+  git diff --exit-code
+fi

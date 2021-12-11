fdd52d770de460fbae32c624a3051c41c1582939
Taiki Endo <te316e89@gmail.com>
2021-12-11 19:12:48 +0900

    Add tools/tidy.sh

--- update-dependabot-pr/main.sh
+++ update-dependabot-pr/main.sh
@@ -17,7 +17,7 @@ fi
 pr_number="${GITHUB_REF#refs/pull/}"
 pr_number="${pr_number%/merge}"
 pr_url="https://api.github.com/repos/${GITHUB_REPOSITORY:?}/pulls/${pr_number}"
-pr_data=$(curl -LsSf -H "${HEADER}" "${pr_url}")
+pr_data=$(curl --proto '=https' --tlsv1.2 -fsSL --retry 10 -H "${HEADER}" "${pr_url}")
 pr_url=$(echo "${pr_data}" | jq -r '.url')
 
 if [[ $(echo "${pr_data}" | jq -r '.user.login') != "dependabot[bot]" ]]; then
@@ -27,7 +27,7 @@ fi
 
 commits_url=$(echo "${pr_data}" | jq -r '.commits_url')
 message=$(
-    curl -sSf "${commits_url}" \
+    curl --proto '=https' --tlsv1.2 -fsSL --retry 10 "${commits_url}" \
         | jq -r '.[0].commit.message' \
         | sed '1,2d' \
         | sed -z 's/\n/\\n/g' \
@@ -39,6 +39,6 @@ if [[ -z "${GITHUB_TOKEN:-}" ]]; then
     exit 1
 fi
 
-curl -sSf -X PATCH -H "${HEADER}" "${pr_url}" \
+curl --proto '=https' --tlsv1.2 -fsSL --retry 10 -X PATCH -H "${HEADER}" "${pr_url}" \
     -H "Authorization: token ${GITHUB_TOKEN}" \
     -d "{ \"body\": \"${message}\" }" >/dev/null

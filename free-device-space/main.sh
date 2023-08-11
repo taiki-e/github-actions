a5d2a8bc7c7b173b13f0169807c6ae9289aa0145
Taiki Endo <te316e89@gmail.com>
2023-08-11 16:27:50 +0900

    free-device-space: rm /usr/local/.ghcup

--- free-device-space/main.sh
+++ free-device-space/main.sh
@@ -6,10 +6,11 @@ IFS=$'\n\t'
 case "${OSTYPE}" in
     linux*)
         # Inspired by https://github.com/easimon/maximize-build-space
-        sudo rm -rf /usr/share/dotnet
-        sudo rm -rf /usr/local/lib/android
         sudo rm -rf /opt/ghc
         sudo rm -rf /opt/hostedtoolcache/CodeQL
+        sudo rm -rf /usr/local/.ghcup
+        sudo rm -rf /usr/local/lib/android
+        sudo rm -rf /usr/share/dotnet
         ;;
     darwin*) ;;
     cygwin* | msys*) ;;

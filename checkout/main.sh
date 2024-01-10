750007af6f2e83df08410cb498e8b5cf506364b8
Taiki Endo <te316e89@gmail.com>
2024-01-10 22:12:13 +0900

    checkout: Install git if unavailable

--- checkout/main.sh
+++ checkout/main.sh
@@ -21,8 +21,107 @@ retry() {
     done
     "$@"
 }
+warn() {
+    echo "::warning::$*"
+}
+apt_update() {
+    if type -P sudo &>/dev/null; then
+        retry sudo apt-get -o Acquire::Retries=10 -qq update
+    else
+        retry apt-get -o Acquire::Retries=10 -qq update
+    fi
+    apt_updated=1
+}
+apt_install() {
+    if [[ -z "${apt_updated:-}" ]]; then
+        apt_update
+    fi
+    if type -P sudo &>/dev/null; then
+        retry sudo apt-get -o Acquire::Retries=10 -o Dpkg::Use-Pty=0 install -y --no-install-recommends "$@"
+    else
+        retry apt-get -o Acquire::Retries=10 -o Dpkg::Use-Pty=0 install -y --no-install-recommends "$@"
+    fi
+}
+apk_install() {
+    if type -P sudo &>/dev/null; then
+        sudo apk --no-cache add "$@"
+    elif type -P doas &>/dev/null; then
+        doas apk --no-cache add "$@"
+    else
+        apk --no-cache add "$@"
+    fi
+}
+dnf_install() {
+    if type -P sudo &>/dev/null; then
+        retry sudo "${dnf}" install -y "$@"
+    else
+        retry "${dnf}" install -y "$@"
+    fi
+}
+sys_install() {
+    case "${base_distro}" in
+        debian) apt_install "$@" ;;
+        alpine) apk_install "$@" ;;
+        fedora) dnf_install "$@" ;;
+    esac
+}
 
 wd=$(pwd)
+
+base_distro=""
+case "$(uname -s)" in
+    Linux)
+        host_os=linux
+        if grep -q '^ID_LIKE=' /etc/os-release; then
+            base_distro=$(grep '^ID_LIKE=' /etc/os-release | sed 's/^ID_LIKE=//')
+            case "${base_distro}" in
+                *debian*) base_distro=debian ;;
+                *alpine*) base_distro=alpine ;;
+                *fedora*) base_distro=fedora ;;
+            esac
+        else
+            base_distro=$(grep '^ID=' /etc/os-release | sed 's/^ID=//')
+        fi
+        case "${base_distro}" in
+            fedora)
+                dnf=dnf
+                if ! type -P dnf &>/dev/null; then
+                    if type -P microdnf &>/dev/null; then
+                        # fedora-based distributions have "minimal" images that
+                        # use microdnf instead of dnf.
+                        dnf=microdnf
+                    else
+                        # If neither dnf nor microdnf is available, it is
+                        # probably an RHEL7-based distribution that does not
+                        # have dnf installed by default.
+                        dnf=yum
+                    fi
+                fi
+                ;;
+        esac
+        ;;
+    Darwin) host_os=macos ;;
+    MINGW* | MSYS* | CYGWIN* | Windows_NT) host_os=windows ;;
+    *) bail "unrecognized OS type '$(uname -s)'" ;;
+esac
+
+if ! type -P git &>/dev/null; then
+    case "${host_os}" in
+        linux*)
+            case "${base_distro}" in
+                debian | fedora | alpine)
+                    echo "::group::Install packages required for installation (git)"
+                    sys_install git
+                    echo "::endgroup::"
+                    ;;
+                *) warn "checkout-action requires git on non-Debian/Fedora/Alpine-based Linux" ;;
+            esac
+            ;;
+        macos) warn "checkout-action requires git on macOS" ;;
+        windows) warn "checkout-action requires git on Windows" ;;
+        *) bail "unsupported host OS '${host_os}'" ;;
+    esac
+fi
 
 g git version
 

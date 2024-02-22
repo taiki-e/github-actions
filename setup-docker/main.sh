c6f879ce070402a9bac5a4de070e2f488c8086cc
Taiki Endo <te316e89@gmail.com>
2024-02-22 18:52:06 +0900

    setup-docker: Retry on docker failure

--- setup-docker/main.sh
+++ setup-docker/main.sh
@@ -14,11 +14,21 @@ g() {
     IFS=$'\n\t'
     "${cmd}" "$@"
 }
+retry() {
+    for i in {1..10}; do
+        if "$@"; then
+            return 0
+        else
+            sleep "${i}"
+        fi
+    done
+    "$@"
+}
 
 g docker version
 g docker buildx version
 
-g docker run --rm --privileged tonistiigi/binfmt --install arm64
+g retry docker run --rm --privileged tonistiigi/binfmt --install arm64
 
 g docker buildx create --name setup-docker-buildx-builder --driver docker-container --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=10485760 --driver-opt network=host --buildkitd-flags --debug --use
 g docker buildx inspect --bootstrap --builder setup-docker-buildx-builder

6b7fc945bce7a5933541cc77774e853799501362
Taiki Endo <te316e89@gmail.com>
2025-01-19 02:59:46 +0900

    setup-docker: Add qemu input option

--- setup-docker/main.sh
+++ setup-docker/main.sh
@@ -26,7 +26,10 @@ retry() {
 g docker version
 g docker buildx version
 
-g retry docker run --rm --privileged tonistiigi/binfmt --install arm64
+case "${INPUT_QEMU:-}" in
+  false) ;;
+  *) g retry docker run --rm --privileged tonistiigi/binfmt --install "${INPUT_QEMU:-}" ;;
+esac
 
 g docker buildx create --name setup-docker-buildx-builder --driver docker-container --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=10485760 --driver-opt network=host --buildkitd-flags --debug --use
 g docker buildx inspect --bootstrap --builder setup-docker-buildx-builder

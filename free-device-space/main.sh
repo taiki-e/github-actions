dcd32bc03a548aed334a719cc7c61b73ffbb28b3
Taiki Endo <te316e89@gmail.com>
2025-03-20 17:49:06 +0900

    free-device-space: Remove more dirs

--- free-device-space/main.sh
+++ free-device-space/main.sh
@@ -13,44 +13,83 @@ case "$(uname -s)" in
     # sometimes takes a more long time.
     # https://github.com/actions/runner-images/issues/1939
     dirs=(
-      /home/linuxbrew           # 164M
-      /home/packer/.dotnet      # 49M
-      /home/runner/.dotnet      # 49M
-      /home/runneradmin/.dotnet # 49M
-      /opt/az                   # 758M
-      # /opt/google/chrome # 342M
+      /home/linuxbrew             # 164M
+      /home/packer/.dotnet        # 49M
+      /home/runner/.dotnet        # 51M
+      /home/runneradmin/.dotnet   # 51M
+      /opt/actionarchivecache     # 209M
+      /opt/az                     # 763M
+      /opt/google-cloud-sdk       # 397M
+      /opt/google/chrome          # 342M
       /opt/hostedtoolcache/CodeQL # 5.1G
       /opt/microsoft/msedge       # 565M
       /opt/microsoft/powershell   # 174M
-      # /usr/lib/firefox # 257M
+      # /opt/pipx # 513M
+      # /opt/pipx/venvs # 500M
+      /usr/include/php          # 5.7M
+      /usr/lib/dotnet           # 508M
+      /usr/lib/firefox          # 238M
       /usr/lib/google-cloud-sdk # 909M
       /usr/lib/heroku           # 280M
       /usr/lib/jvm              # 1.2G
       /usr/lib/linux-azure-*    # 14M
-      # /usr/lib/llvm-13 # 448M
-      # /usr/lib/llvm-14 # 486M
-      # /usr/lib/llvm-15 # 514M
-      /usr/lib/mono    # 423M
-      /usr/lib/monodoc # 22M
-      /usr/lib/nuget   # 7.0M
-      # /usr/local/aws-cli     # 226M
-      # /usr/local/aws-sam-cli # 173M
-      /usr/local/.ghcup      # 5.5G
+      /usr/lib/mono             # 423M
+      /usr/lib/monodoc          # 22M
+      /usr/lib/mysql            # 6.5M
+      /usr/lib/nuget            # 7.0M
+      /usr/lib/php              # 23M
+      /usr/local/.ghcup         # 5.5G
+      # /usr/local/aws-cli     # 248M
+      # /usr/local/aws-sam-cli # 179M
+      /usr/local/doc         # 51M
       /usr/local/julia*      # 602M
       /usr/local/lib/android # 7.6G
-      # /usr/local/lib/node_modules # 1.1G
+      # /usr/local/lib/node_modules # 635M
+      /usr/local/man              # 4.1M
+      /usr/local/n                # 166M
       /usr/local/share/chromium   # 535M
       /usr/local/share/powershell # 1.2G
-      /usr/share/az_*             # 467M
+      /usr/share/az_*             # 468M
+      /usr/share/doc              # 64M
       /usr/share/dotnet           # 1.6G
       /usr/share/gradle-*         # 146M
-      /usr/share/java             # 40M
+      /usr/share/groovy           # 14M
+      /usr/share/i18n             # 16M
+      /usr/share/java             # 131M
       /usr/share/kotlinc          # 91M
+      /usr/share/man              # 36M
+      /usr/share/mecab            # 52M
       /usr/share/miniconda        # 658M
+      /usr/share/mysql            # 9.4M
       /usr/share/R                # 11M
+      /usr/share/ri               # 56M
       /usr/share/sbt              # 138M
-      /usr/share/swift            # 2.6G
+      /usr/share/swift            # 2.5G
+      /var/lib/mecab              # 91M
+      /var/lib/mysql              # 180M
     )
+    # ubuntu-22.04
+    if [[ -e /usr/lib/llvm-15 ]]; then
+      dirs+=(
+        /usr/include/llvm-13 # 24M
+        /usr/include/llvm-14 # 24M
+        # /usr/include/llvm-15 # 25M
+        /usr/lib/llvm-13 # 448M
+        /usr/lib/llvm-14 # 486M
+        # /usr/lib/llvm-15 # 514M
+      )
+    fi
+    # ubuntu-24.04
+    if [[ -e /usr/lib/llvm-18 ]]; then
+      dirs+=(
+        /usr/include/llvm-16 # 26M
+        /usr/include/llvm-17 # 27M
+        # /usr/include/llvm-18 # 28M
+        /usr/lib/llvm-16 # 558M
+        /usr/lib/llvm-17 # 564M
+        # /usr/lib/llvm-18 # 697M
+      )
+    fi
     for dir in "${dirs[@]}"; do
       if [[ ! -d "${dir}" ]]; then
         continue
